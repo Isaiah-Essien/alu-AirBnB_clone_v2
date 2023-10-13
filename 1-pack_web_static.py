b7222cb6be5940dcc4a57d2d7ebc6d8fb92cf515
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2023-10-13 16:34:32 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 This script to generate .tgz archive for contents of web_static folder
 """
@@ -7,13 +7,13 @@ from fabric.api import local
 
 
 def do_pack():
-        """
-    Yoo,this generates .tgz archive from contents of web_static
     """
-            local("mkdir -p versions")
-                current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-                    path = "versions/web_static_{}.tgz".format(current_time)
-                        result = local("tar -czf {} web_static/".format(path))
-                            if result.failed:
-                                        return None
-                                        return path
+    Yoo, this generates .tgz archive from contents of web_static
+    """
+    local("mkdir -p versions")
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(current_time)
+    result = local("tar -czf {} web_static/".format(path))
+    if result.failed:
+        return None
+    return path
